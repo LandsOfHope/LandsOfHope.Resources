68616d38c5be5766307894b041a8efc91924975d
Martyn York <etinquis@gmail.com>
2023-08-08 12:57:13 -0700

    add chat color css generation

---
+++ .build/generate_chat_colors_css.mjs
@@ -0,0 +1,19 @@
+import { mkdirSync, writeFileSync } from 'fs';
+import fetch from 'node-fetch'
+
+const main = async () => {
+    const chatColorResponse = await (await fetch("https://data.landsofhope.com/schemas/v1/chat/chat-color.json")).json();
+    const enumValues = chatColorResponse.enum;
+
+    let css = "";
+    for (var i = 0; i < enumValues.length; ++i) {
+        const color = enumValues[i];
+
+        css += `\r.chat-color-${color} { color: #${color}; } .bg-chat-color-${color} { background-color: #${color} }`;
+    }
+
+    mkdirSync('css/chat', { recursive: true });
+    writeFileSync('css/chat/chat-colors.css', css);
+}
+
+main();
