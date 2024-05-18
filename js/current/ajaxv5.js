9a039b690952838f9aa0db922cd1e36f99265353
Martyn York <etinquis@gmail.com>
2024-05-18 13:32:17 -0700

    replace xmlhttprequest with fetch

--- js/current/ajaxv5.js
+++ js/current/ajaxv5.js
@@ -6,15 +6,15 @@ var chatrefresht = 0;
 var pmrefresht = 0;
 var lastpmrefresh = '';
 var lastchatrefresh = '';
-function ajaxerror(e1, et, e3) {
+function ajaxerror(url, e1, et, e3) {
 	if (e1 == null && et == null) {
 		window.top.SendCommand('Your browser may not support AJAX (' + e3 + '), this may mean the game will not function correctly. Try going to File > Chat Options and turning off chat for improved performance.');
 	} else {
 		if (window.top.ErrorsOn != 0) {
 			if (e1 == 12029) {
-				window.top.SendCommand('Chat Error: The chat system is currently unable to connect to the chat server, please check you are still connected to the internet. Chat will attempt to connect automatically every few seconds.');
+				window.top.SendCommand('AJAX Error: The chat system is currently unable to connect to the chat server, please check you are still connected to the internet. Chat will attempt to connect automatically every few seconds.');
 			} else {
-				window.top.SendCommand('Chat Error at ' + e3 + ': ' + e1 + ', ' + et);
+				window.top.SendCommand(`AJAX Error (${url}) at ${e3}: ${e1}, ${et}`);
 			}
 		}
 	}
@@ -31,27 +31,21 @@ function getXmlHttpRequestObject() {
 	}
 }
 
-function sendRequest(doc) {
-	if (xmlobj != null) {
-		// open socket connection
-		xmlobj.open('GET', doc, true);
-		// send GET request
-		xmlobj.setRequestHeader("Cache-Control", "no-cache");
-		xmlobj.onreadystatechange = function () {
-
-			if (xmlobj.readyState == 4 && xmlobj.status == 200) {
-
-				if (xmlobj.responseText) {
-					var r = eval(xmlobj.responseText);
-				}
-			} else if (xmlobj.readyState == 4 && xmlobj.status != 200) {
-				ajaxerror(xmlobj.status, xmlobj.statusText, 'Routine 1b');
-			}
-		};
-
-
-		xmlobj.send(null);
-	} else {
-		ajaxerror(null, null, 'Routine 1 in ' + doc);
-	}
+async function sendRequest(doc) {
+	await fetch(doc, {
+		method: 'GET',
+		headers: {
+			'Cache-Control': 'no-cache'
+		}
+	}).then(async response => {
+		if (response.ok) {
+            const responseText = await response.text();
+            console.log(responseText);
+			eval(responseText);
+		} else {
+			ajaxerror(doc, response.status, response.statusText, 'Routine 1b');
+		}
+	}).catch(error => {
+		ajaxerror(doc, null, null, 'Routine 1 in ' + doc);
+	});
 }
