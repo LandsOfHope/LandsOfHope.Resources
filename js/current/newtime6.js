812a2cbd682cd64ce0bec91b4853be05f3115e21
Martyn York <etinquis@gmail.com>
2024-07-05 11:32:44 -0700

    day/night cycle

--- js/current/newtime6.js
+++ js/current/newtime6.js
@@ -86,36 +86,36 @@ function gethourpic() {
 	}
 }
 
-function getLight() {
-	var now3;
-	now3 = getfhtime();
-	if (now3.getHours() < 4) {
-		return 80;
-	}
-	else if (now3.getHours() < 7) {
-		return 85;
-	}
-	else if (now3.getHours() < 10) {
-		return 90;
-	}
-	else if (now3.getHours() < 12) {
-		return 95;
-	}
-	else if (now3.getHours() < 15) {
-		return 100;
-	}
-	else if (now3.getHours() < 17) {
-		return 95;
-	}
-	else if (now3.getHours() < 18) {
-		return 90;
-	}
-	else if (now3.getHours() < 20) {
-		return 85;
-	}
-	else {
-		return 80;
-	}
+function getDaylightBrightness(time) {
+    const totalMinutes = time.getHours() * 60 + time.getMinutes();
+
+    // Convert 5AM and 8PM to minutes
+    const morningThreshold = 5 * 60; // 5AM in minutes
+    const eveningThreshold = 20 * 60; // 8PM in minutes
+    const noon = 12 * 60; // 12 noon in minutes
+
+    // Before 5AM or after 8PM, brightness is 0
+    if (totalMinutes < morningThreshold || totalMinutes > eveningThreshold) {
+        return 0;
+    }
+    // At 12 noon, brightness is 1
+    else if (totalMinutes === noon) {
+        return 1;
+    }
+    // Calculate brightness for times between 5AM and 12 noon
+    else if (totalMinutes > morningThreshold && totalMinutes < noon) {
+        // Linearly increase from 0 to 1 as time goes from 5AM to 12 noon
+        return (totalMinutes - morningThreshold) / (noon - morningThreshold);
+    }
+    // Calculate brightness for times between 12 noon and 8PM
+    else if (totalMinutes > noon && totalMinutes <= eveningThreshold) {
+        // Linearly decrease from 1 to 0 as time goes from 12 noon to 8PM
+        return (eveningThreshold - totalMinutes) / (eveningThreshold - noon);
+    }
+    // Default case to handle any unexpected input
+    else {
+        return 0;
+    }
 }
 
 function TimeTip() {
@@ -132,7 +132,10 @@ function refreshTime() {
 		window.top.getObj('timebar').style.backgroundImage = "URL(https://lohcdn.com/game/icons/" + gethourpic() + "s.png)";
 		window.top.getObj('timebar').innerHTML = '' + window.top.ASCII('<small>Myzan Time:</small>', 1) + ' ' + window.top.ASCII('<b>' + getstime() + '</b>', 1);
 	}
-	var localti = window.top.timeindex;
+
+	const daylightBrightness = getDaylightBrightness(getfhtime());
+	window.top.getObj("Map2").style.setProperty("--mapTimeFilter", `sepia(${daylightBrightness * 100}%) brightness(${(1 - daylightBrightness) * 70}%)`);
+
 	if (tmok == 1) {
 		timer = setTimeout(refreshTime, 10000);
 	}
