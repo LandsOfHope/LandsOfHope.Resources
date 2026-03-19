3f6f6cf93024fc1d34819be8f60606aab576aba0
Martyn York <etinquis@gmail.com>
2026-03-19 15:11:22 -0700

    pseudo scoped cdn_resources_url for rcsbars, submodalv3

--- js/current/rcsbars.js
+++ js/current/rcsbars.js
@@ -2,7 +2,7 @@ var rcsbuttons = new Array();
 var hs = 0;
 var mastervalue = 0;
 var PageLoaded = PageLoaded;
-var CDN_RESOURCES_URL = new URL(document.currentScript.src).origin;
+const CDN_RESOURCES_URL_rcsbars = new URL(document.currentScript.src).origin;
 
 const HIDE_TIMEOUT_MS = 5000;
 
@@ -107,7 +107,7 @@ function ShowSubMenu(b, divx, mv) {
 	var x = 0;
 	if (b != -1) {
 		for (x = 0; x < rcsbuttons[b].submenus.length; x++) {
-			strout += `<div id="rcssm${x}" style="margin: 2px; padding: 1px; border: 1px solid RGB(86, 78, 76); background-Color: RGB(86, 78, 76); cursor: pointer;" onmouseover="RCSSMOver(${b},${x});" onclick="RCSSMClick(${b},${x});" onmouseout="RCSSMOut(${b},${x});"><table cellpadding=0 cellspacing=0 style="font-family: Helvetica; font-size: 8pt;"><tr><td style="width: 20px;" id="prcssm${x}"><img src='${CDN_RESOURCES_URL}/images/circle0a.png' width=12 height=12></td><td style="color:white;">${rcsbuttons[b].submenus[x].text}</td></tr></table></div>`;
+			strout += `<div id="rcssm${x}" style="margin: 2px; padding: 1px; border: 1px solid RGB(86, 78, 76); background-Color: RGB(86, 78, 76); cursor: pointer;" onmouseover="RCSSMOver(${b},${x});" onclick="RCSSMClick(${b},${x});" onmouseout="RCSSMOut(${b},${x});"><table cellpadding=0 cellspacing=0 style="font-family: Helvetica; font-size: 8pt;"><tr><td style="width: 20px;" id="prcssm${x}"><img src='${CDN_RESOURCES_URL_rcsbars}/images/circle0a.png' width=12 height=12></td><td style="color:white;">${rcsbuttons[b].submenus[x].text}</td></tr></table></div>`;
 		}
 	}
 	strout += '';
@@ -194,7 +194,7 @@ function RCSSMOver(b, x) {
 		bb = null;
 		var bb2 = getObj('prcssm' + x);
 		if (bb2 != null) {
-			bb2.innerHTML = `<img src='${CDN_RESOURCES_URL}/images/circle6.png' width=12 height=12>`
+			bb2.innerHTML = `<img src='${CDN_RESOURCES_URL_rcsbars}/images/circle6.png' width=12 height=12>`
 		}
 		bb2 = null;
 
@@ -212,7 +212,7 @@ function RCSSMOut(b, x) {
 		}
 		var bb2 = getObj('prcssm' + x);
 		if (bb2 != null) {
-			bb2.innerHTML = `<img src='${CDN_RESOURCES_URL}/images/circle0a.png' width=12 height=12>`
+			bb2.innerHTML = `<img src='${CDN_RESOURCES_URL_rcsbars}/images/circle0a.png' width=12 height=12>`
 		}
 		bb2 = null;
 	}
@@ -234,7 +234,7 @@ function RCSSMClick(b, x) {
 		}
 		var bb2 = getObj('prcssm' + x);
 		if (bb2 != null) {
-			bb2.innerHTML = `<img src='${CDN_RESOURCES_URL}/images/circle3.png' width=12 height=12>`;
+			bb2.innerHTML = `<img src='${CDN_RESOURCES_URL_rcsbars}/images/circle3.png' width=12 height=12>`;
 		}
 		bb2 = null;
 	}
